b6c686d5d1ca1c12d0f03616735fcb8bc478a0d9
Jiyuu109 <happymapjyk@gmail.com>
2026-09-03 09:50:08 +0900

    add 03_control files

---
+++ 03_control/ex2.py
@@ -0,0 +1,48 @@
+# 반복문 : while문, for문
+
+# while문
+# 1 ~ 10까지 반복 출력
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+    if i == 6:
+        break
+else:
+    print("End")
+
+nums = [1, 3, 5, 7, 9]
+target = 2
+i = 0
+
+while i < len(nums):
+    if nums[i] == target:
+        print(f"{target} found.")
+        break
+    i += 1
+else:
+    print(f"{target} not found")
+
+# 1 ~ 10까지의 합
+i = 1
+tot = 0
+while i <= 10:
+    tot += i
+    i += 1
+print(f"sum = {tot}")
+
+# 1 ~ 10까지의 합 (짝수)
+i = 1
+tot = 0
+while i <= 10:
+    if i % 2 == 0:
+        tot += i
+    i += 1
+print(f"sum = {tot}")
+
+while i <= 10:
+    i += 1
+    if i % 2 == 0:
+        continue
+    tot += i
+print(f"sum = {tot}")
